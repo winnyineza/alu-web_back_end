fc79dd14dd4dd031803c56eeae71aac3bdfd85d3
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2024-10-19 21:11:46 +0200

    done

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python3
-""" Implementing an expiring web cache and tracker
-    obtain the HTML content of a particular URL and returns it """
-import redis
+""" Redis with requests """
 import requests
+from functools import wraps
+from typing import Callable
+import redis
 
-# Initialize Redis connection
-r = redis.Redis()
 
+def count(method: Callable):
+    """ Count the call to requests """
+    r = redis.Redis()
+
+    @wraps(method)
+    def wrapped(url):
+        """ function that will count """
+        r.incr(f"count:{url}")
+        expiration_count = r.get(f"cached:{url}")
+        if expiration_count:
+            return expiration_count.decode('utf-8')
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapped
+
+
+@count
 def get_page(url: str) -> str:
-    """ Track how many times a particular URL was accessed in the key
-        "count:{url}" and cache the result with an expiration time of 10 seconds """
-    
-    # Increment the access count for the URL
-    r.incr(f"count:{url}")
-    
-    # Check if the URL is already cached
-    cached_response = r.get(f"cached:{url}")
-
-    if cached_response:
-        # Return the cached response if it exists
-        return cached_response.decode('utf-8')
-
-    # If not cached, fetch the page
-    resp = requests.get(url)
-
-    # Cache the response with an expiration time of 10 seconds
-    r.setex(f"cached:{url}", 10, resp.text)
-
-    return resp.text
-
-if __name__ == "__main__":
-    # Test the function with the provided URL
-    print(get_page('http://slowwly.robertomurray.co.uk/delay/10/url/http://example.com'))
+    """ module to obtain the HTML """
+    return requests.get(url).text
