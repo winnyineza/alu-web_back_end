691c564050a3d56a8c0e49243b1b67d8d8f7f060
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2024-10-26 11:18:08 +0200

    done

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python3
-"""Provides stats about Nginx logs stored in MongoDB"""
+"""
+Script that provides some stats about Nginx logs stored in MongoDB.
+"""
 from pymongo import MongoClient
 
 
-def log_stats(mongo_collection):
-    """Prints stats about Nginx logs stored in MongoDB"""
-    total_logs = mongo_collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = mongo_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    status_check = mongo_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_check} status check")
-
-
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collection = db.nginx
+    client = MongoClient('mongodb://localhost:27017')
+    collec = client.logs.nginx
 
-    log_stats(collection)
+    print(f'{collec.count_documents({})} logs')
+    print('Methods:')
+
+    print(f'\tmethod GET: {collec.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {collec.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {collec.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {collec.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {collec.count_documents({"method": "DELETE"})}')
+    print(f'{collec.count_documents({"path": "/status"})} status check')
