17062b35ec1c8f49c7b10988227eeb0bd4bf65bd
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2024-11-12 12:41:02 +0200

    made changes

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,36 +1,52 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  const promise = (res, rej) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) rej(Error('Cannot load the database'));
-      const messages = [];
-      let message;
-      const content = data.toString().split('\n');
-      let students = content.filter((item) => item);
-      students = students.map((item) => item.split(','));
-      const nStudents = students.length ? students.length - 1 : 0;
-      message = `Number of students: ${nStudents}`;
-      console.log(message);
-      messages.push(message);
-      const subjects = {};
-      for (const i in students) {
-        if (i !== 0) {
-          if (!subjects[students[i][3]]) subjects[students[i][3]] = [];
-          subjects[students[i][3]].push(students[i][0]);
+module.exports = function countStudents(path) {
+  return new Promise(((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, params) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+
+      let s = params;
+      s = s.split('\n');
+      const headers = s.shift().split(',');
+
+      const groups = {};
+      const studentsObjects = [];
+
+      s.forEach((student) => {
+        if (student) {
+          const info = student.split(',');
+          const studentObject = {};
+
+          headers.forEach((header, index) => {
+            studentObject[header] = info[index];
+            if (header === 'field') {
+              if (groups[info[index]]) {
+                groups[info[index]].push(studentObject.firstname);
+              } else {
+                groups[info[index]] = [studentObject.firstname];
+              }
+            }
+          });
+          studentsObjects.push(studentObject);
+        }
+      });
+      const number = `Number of students: ${studentsObjects.length}`;
+
+      let response = `${number}\n`;
+      console.log(number);
+
+      for (const i in groups) {
+        if (groups[i]) {
+          const list = groups[i];
+          const j = `Number of students in ${i}: ${list.length}. List: ${list.join(', ')}`;
+          response += `${j}\n`;
+          console.log(j);
         }
       }
-      delete subjects.subject;
-      for (const key of Object.keys(subjects)) {
-        message = `Number of students in ${key}: ${
-          subjects[key].length
-        }. List: ${subjects[key].join(', ')}`;
-        console.log(message);
-        messages.push(message);
-      }
-      res(messages);
+      resolve(response);
     });
-  };
-  return new Promise(promise);
-}
-module.exports = countStudents;
+  }));
+};
