77fa9ac6a8112c5accd11caec16805e2a368e938
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2024-11-12 12:43:05 +0200

    update task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,20 +1,31 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const students = require('./3-read_file_async');
 
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
-  if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
-    }
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  switch (req.url) {
+    case '/':
+      res.writeHead(200);
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      res.writeHead(200);
+      res.write('This is the list of our students\n');
+      students(process.argv[2])
+        .then((data) => {
+          res.end(data);
+        })
+        .catch((error) => {
+          res.end(error.message);
+        });
+      break;
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: 'Resource not found' }));
   }
-  res.end();
 });
-app.listen(1245);
+
+app.listen(port);
+
 module.exports = app;
