4dbb4d52349de264ca218b83374e123bd47df006
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2024-11-12 12:45:24 +0200

    changed Task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const app = express();
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
-app.get('/students', async (req, res) => {
-  const title = 'This is the list of our students\n';
-  try {
-    const data = await countStudents(process.argv[2]);
-    res.send(`${title}${data.join('\n')}`);
-  } catch (error) {
-    res.send(`${title}${error.message}`);
-  }
+const port = 1245;
+
+const app = express().get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
-app.listen(1245);
+
+app.get('/students', (req, res) => {
+  res.write('This is the list of our students\n');
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.end(data);
+    })
+    .catch((error) => {
+      res.end(error.message);
+    });
+});
+
+app.listen(port);
+
 module.exports = app;
